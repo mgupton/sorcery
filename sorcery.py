ece2cf4ec4aa0a735e6db63c01cc11b3f3648e0d
Michael Gupton <mgupton@alertlogic.com>
2017-12-19 10:20:34 -0600

    - Fixed data type mismatch in assign_me.

--- sorcery.py
+++ sorcery.py
@@ -799,7 +799,7 @@ def assign_me(api_key, cid, policy_name):
 
         policy_id = get_assignment_policy_id(api_key, cid, policy_name)
 
-        post_data = '{"protectedhost": {"policy": {"id": "%d"}}' % (policy_id)
+        post_data = '{"protectedhost": {"policy": {"id": "%s"}}' % (policy_id)
 
         api_endpoint += "%s" % (phost_id)
 
